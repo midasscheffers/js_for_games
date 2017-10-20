37435143537d8370c7344b7191b549bf954e4091
Midas Scheffers <midasscheffers@gmail.com>
2017-10-20 12:57:11 +0200

    Make background turn red if player touches balls and add localstorige

--- draw.js
+++ draw.js
@@ -13,17 +13,26 @@ var player_size = 50;
 var life = 250;
 var stage = 0;
 var timeStart;
+var lose_life = false;
+var highscore;
+var eerder = localStorage.getItem("eerder");
+if (eerder != "ja"){
+  localStorage.setItem("eerder", "ja");
+  localStorage.setItem("highscore", 0);
+}
+highscore = localStorage.getItem("highscore");
 
 function setup(){
   createCanvas(xSize, ySize);
   noCursor();
   for (var i = 0; i< aantalBallen; i++){
-    bal = new Bal(random(60,innerWidth - 60), random(60, innerHeight - 60), ball_radius + random(0,25), random(1, 6), random(1, 6),[random(255), random(255), random(255)]);
+    bal = new Bal(random(60,innerWidth - 60), random(60, innerHeight - 60), ball_radius + random(0,25), random(1, 3), random(1, 3),[random(255), random(255), random(255)]);
     ballen.push(bal);
   }
 }
 
 function draw(){
+  strokeWeight(3);
   if(stage == 0){
     background(200);
     fill(170);
@@ -42,26 +51,45 @@ function draw(){
       fill(150,0,0);
       noStroke();
       text("You died", (innerWidth/2)-50, (innerHeight/2)-25);
-      fill(170)
+      fill(170);
       stroke(50);
       ellipse(mouseX,mouseY,10);
       noStroke();
       textSize(32);
       fill(50,255,50);
       text("score: " + score.toString(), 150, 30);
+      if (score > highscore){
+        localStorage.setItem("highscore", score);
+        highscore = score;
+      }
+      text("highscore: " + highscore.toString(), 500, 30);
     }
     else{
       // time logic
       score = floor(millis() - timeStart);
-      background(255,255,255,speed_force)
+      if (lose_life == false){
+        background(255,255,255,speed_force);
+      }else if(lose_life == true){
+        background(255,0,0,speed_force);
+      }
       // print life
       textSize(32);
-      fill(255,0,0);
+      if (lose_life == false){
+        fill(255,0,0);
+      }else if(lose_life == true){
+        fill(0);
+      }
       text("life: " + life, 0, 30);
       fill(50,255,50);
       text("score: " + score.toString(), 150, 30);
       fill(50);
-      noStroke();
+      if (lose_life == false){
+        noStroke();
+      }else if(lose_life == true){
+        strokeWeight(3);
+        stroke(255,0,0);
+      }
+      lose_life = false;
       ellipse(mouseX, mouseY, player_size);
       //ball logic
       for (var i = 0; i < ballen.length; i++){
@@ -93,7 +121,6 @@ function Bal(x, y, radius, xspd, yspd, ball_col){
 
   this.teken = function(){
     noStroke();
-    //stroke(0);
     fill(ball_col);
     ellipse(this.xPos, this.yPos, 2*this.radius, 2*this.radius);
   }
@@ -102,6 +129,7 @@ function Bal(x, y, radius, xspd, yspd, ball_col){
     dx = mouseX - this.xPos;
     dy = mouseY - this.yPos;
     if (sqrt(dx*dx + dy*dy) <= player_size + this.radius){
+      lose_life = true;
       life -= floor(this.radius/10) + 1;
     }
   }
@@ -109,14 +137,20 @@ function Bal(x, y, radius, xspd, yspd, ball_col){
   this.beweeg = function(){
     if (this.xPos > width - this.radius || this.xPos < this.radius){
       if (this.xSpeed < 0){
-        this.xSpeed -=0.1;
+        this.xSpeed -=0.5;
       }
       else{
-        this.xSpeed +=0.1;
+        this.xSpeed +=0.5;
       }
       this.xSpeed = -this.xSpeed;
     }
     if (this.yPos > height - this.radius || this.yPos < this.radius){
+      if (this.ySpeed < 0){
+        this.ySpeed -=0.5;
+      }
+      else{
+        this.ySpeed +=0.5;
+      }
       this.ySpeed = -this.ySpeed;
     }
     this.xPos += this.xSpeed;
